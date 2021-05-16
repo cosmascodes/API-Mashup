9fcf97a676342d6ff4038b154f74b7434a953e9a
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2021-05-16 10:51:26 +0300

    m

--- static/js/resources.js
+++ static/js/resources.js
@@ -1,47 +1,87 @@
+var config = {
+  client_id: "06fef78a0beb0092952c236091a4ae07",
+  client_secret: "b109eb416300214b66234aec115f8d11cb7ec122b151864cea8ece58034eb95b",
+  redirect_uri: "https://priceless-ride-34b555.netlify.app/",
+  authorization_endpoint: "https://api.myanimelist.net/v2/anime",
+  token_endpoint: "",
+  requested_scopes: ""
+};
+
 let btn = document.getElementById('submit')
 
 btn.addEventListener('click',(e)=>{
   e.preventDefault()
+  let name = document.getElementById('name').value
+  let age = document.getElementById('age').value
 
-  // Create a request variable and assign a new XMLHttpRequest object to it.
-let request = new XMLHttpRequest()
+  const app = document.getElementById('root')
+  const container = document.createElement('div')
+  container.setAttribute('class', 'container')
+  app.appendChild(container)
+  const ercard = document.createElement('div')
+  ercard.setAttribute('class', 'error')
 
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+  if(name!="" && age!=""){
+    let form = document.getElementById('form')
+    form.style.display="none"
+      // Create a request variable and assign a new XMLHttpRequest object to it.
+      let request = new XMLHttpRequest()
 
-const app = document.getElementById('root')
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
-app.appendChild(container)
+      // Open a new connection, using the GET request on the URL endpoint
+      request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
 
-request.onload = function () {
-  // Begin accessing JSON data here
-let data = JSON.parse(this.response)
+      
+
+      request.onload = function () {
+        // Begin accessing JSON data here
+      let data = JSON.parse(this.response)
 
 
-if (request.status >= 200 && request.status < 400) {
-    data.forEach((movie) => {
-        const card = document.createElement('div')
-        card.setAttribute('class', 'card')
-        // Create an h1 and set the text content to the film's title
-        const h1 = document.createElement('h1')
-        h1.textContent = movie.title
-        // Create a p and set the text content to the film's description
-        const p = document.createElement('p')
-        movie.description = movie.description.substring(0, 300) // Limit to 300 chars
-        p.textContent = `${movie.description}...` // End with an ellipses
+      if (request.status >= 200 && request.status < 400) {
+          data.forEach((movie) => {
+              const card = document.createElement('div')
+              card.setAttribute('class', 'card')
+              // Create an h1 and set the text content to the film's title
+              const h1 = document.createElement('h1')
+              const p = document.createElement('p')
+              const d_name = document.createElement('p')
 
-        // Append the cards to the container element
-        container.appendChild(card)
 
-        // Each card will contain an h1 and a p
-        card.appendChild(h1)
-        card.appendChild(p)
-    })
-  } else {
-    console.log('error')
+              h1.textContent = movie.title        
+              movie.description = movie.description.substring(0, 300) // Limit to 300 chars
+              p.textContent = `${movie.description}...` // End with an ellipses
+              d_name.textContent = `Director: ${movie.director}.`
+
+              // Append the cards to the container element
+              container.appendChild(card)
+
+              // Each card will contain an h1 and a p
+              card.appendChild(h1)
+              card.appendChild(p)
+              card.appendChild(d_name)
+          })
+        } else {
+          console.log('error')
+        }
+      }
+      // Send request
+      request.send()
+  }else{
+    let e = document.createElement('p')
+    container.appendChild(ercard)
+    
+    if(name==""&&age==""){      
+      e.textContent = "Please enter your name and age"
+      ercard.appendChild(e)
+    }
+    if(name==""&&age!==""){
+      e.textContent = "Please enter your name"
+      ercard.appendChild(e)
+    }
+    if(name!==""&&age==""){
+      e.textContent = "Please enter your age"
+      ercard.appendChild(e)
+    }
+
   }
-}
-// Send request
-request.send()
 })
